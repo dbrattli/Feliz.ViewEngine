a88c4bddcbfe69f05c8accac96ba7f064ab12114
Dag Brattli <dag.brattli@cognite.com>
2020-04-19 18:49:29 +0200

    Add basic tests

--- test/ViewEngineTest.fs
+++ test/ViewEngineTest.fs
@@ -5,10 +5,61 @@ open Swensen.Unquote
 open Xunit
 
 [<Fact>]
-let ``Text element is Ok``() =
+let ``Simple text element is Ok``() =
+    // Arrange / Act
     let result =
         Html.text "test"
         |> Render.htmlNode
 
     // Assert
     test <@ result = "test" @>
+
+[<Fact>]
+let ``Simple p element with text is Ok``() =
+    // Arrange / Act
+    let result =
+        Html.p "test"
+        |> Render.htmlNode
+
+    // Assert
+    test <@ result = "<p>test</p>" @>
+
+[<Fact>]
+let ``Simple p element with text property is Ok``() =
+    // Arrange / Act
+    let result =
+        Html.p [
+            prop.text "test"
+        ]
+        |> Render.htmlNode
+
+    // Assert
+    test <@ result = "<p>test</p>" @>
+
+[<Fact>]
+let ``Simple p element with text element is Ok``() =
+    // Arrange / Act
+    let result =
+        Html.p [
+            Html.text "test"
+        ]
+        |> Render.htmlNode
+
+    // Assert
+    test <@ result = "<p>test</p>" @>
+
+
+[<Fact>]
+let ``Simple p element with text element and class property is Ok``() =
+    // Arrange / Act
+    let result =
+        Html.p [
+            prop.className "main"
+            prop.children [
+                Html.text "test"
+            ]
+        ]
+        |> Render.htmlNode
+
+    // Assert
+    test <@ result = "<p class=\"main\">test</p>" @>
