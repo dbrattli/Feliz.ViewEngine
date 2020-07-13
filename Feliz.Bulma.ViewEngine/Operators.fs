b97ee665e8836361aa7137e804923688c96198ab
Dag Brattli <dag.brattli@cognite.com>
2020-07-13 11:10:32 +0200

    Move Feliz.Bulma.ViewEngine to separate repo

--- Feliz.Bulma.ViewEngine/Operators.fs
+++
@@ -1,7 +0,0 @@
-﻿module Feliz.Bulma.ViewEngine.Operators
-
-open Feliz.ViewEngine
-
-let (++) (prop1: IReactProperty) (prop2: IReactProperty) =
-    ElementBuilders.Helpers.getClasses [prop1; prop2]
-    |> prop.classes
--- Feliz.Bulma.ViewEngine/PropertyBuilders.fs
+++
@@ -1,8 +0,0 @@
-﻿module internal Feliz.Bulma.ViewEngine.PropertyBuilders
-
-open Feliz.ViewEngine
-
-let inline mkClass (value:string) = Interop.mkAttr "class" value
-let inline mkType (value:string) = Interop.mkAttr "type" value
-let inline mkValue (value:int) = Interop.mkAttr "value" value
-let inline mkMax (value:int) = Interop.mkAttr "max" value
