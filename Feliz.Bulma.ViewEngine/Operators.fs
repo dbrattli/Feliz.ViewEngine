472e92544e630bb7c4076df01d756c1f24be0047
Dag Brattli <dag.brattli@cognite.com>
2020-10-10 15:53:59 +0200

    Revert removal of bulma code

---
+++ Feliz.Bulma.ViewEngine/Operators.fs
@@ -0,0 +1,7 @@
+﻿module Feliz.Bulma.ViewEngine.Operators
+
+open Feliz.ViewEngine
+
+let (++) (prop1: IReactProperty) (prop2: IReactProperty) =
+    ElementBuilders.Helpers.getClasses [prop1; prop2]
+    |> prop.classes
---
+++ Feliz.Bulma.ViewEngine/PropertyBuilders.fs
@@ -0,0 +1,8 @@
+﻿module internal Feliz.Bulma.ViewEngine.PropertyBuilders
+
+open Feliz.ViewEngine
+
+let inline mkClass (value:string) = Interop.mkAttr "class" value
+let inline mkType (value:string) = Interop.mkAttr "type" value
+let inline mkValue (value:int) = Interop.mkAttr "value" value
+let inline mkMax (value:int) = Interop.mkAttr "max" value
