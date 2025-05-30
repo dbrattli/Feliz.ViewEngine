c1192738c4f0fa1544ed8c44471b1dba9ecd0cd9
jkone27 <jkone27@yahoo.it>
2025-05-30 14:55:07 +0200

    adjust push script

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@ echo "Please enter your NuGet API key:"
 read -s NUGET_API_KEY
 echo "Pushing package to NuGet..."
 
-dotnet nuget push ./Feliz.ViewEngine.Jkone27.0.25.0-alpha.1.nupkg --source https://api.nuget.org/v3/index.json --api-key "$NUGET_API_KEY"
+dotnet nuget push ./*.nupkg --source https://api.nuget.org/v3/index.json --api-key "$NUGET_API_KEY"
